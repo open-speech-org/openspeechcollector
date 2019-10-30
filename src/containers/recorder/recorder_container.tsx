439d1888f6bdc14f05daaf41baef31f694ca22c8
Mauricio Collazos <ma0@contraslash.com>
2019-10-29 23:17:31 -0500

    remove eject

---
+++ src/containers/recorder/recorder_container.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RecorderContainer extends React.Component {
+    render(): React.ReactElement{
+        return (
+            <div>
+                Hello world
+            </div>
+        );
+    }
+}
+
+export default RecorderContainer
